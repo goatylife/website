977e56604c2538b310f1860bfc418309960075fc
goaty <190173149+goatylife@users.noreply.github.com>
2024-12-02 14:35:50 +0900

    Initialize web application via create-cloudflare CLI
    Details:
      C3 = create-cloudflare@2.33.1
      project name = website
      framework = astro
      framework cli = create-astro@4.10.0
      package manager = bun@1.1.38
      wrangler = wrangler@3.91.0
      git = 2.47.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,16 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap()],
+  output: 'server',
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true
+    }
+  }),
 });
